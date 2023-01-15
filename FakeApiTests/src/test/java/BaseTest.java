d07c85c4b3d9d5c0b25ec9b68e5fcacad29bd5e3
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-01-16 00:13:14 +0100

    another commit

---
+++ FakeApiTests/src/test/java/BaseTest.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    protected RequestSpecification reqSpec;
+    protected ResponseSpecification resSpec;
+
+    @BeforeClass
+    public void setup() {
+
+        reqSpec = new RequestSpecBuilder().setBaseUri("http://localhost:3000")
+                .setBasePath("posts")
+                .setContentType(ContentType.JSON)
+                .build();
+
+        resSpec = new ResponseSpecBuilder().expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+
+        RequestLoggingFilter reqLog = new RequestLoggingFilter();
+        ResponseLoggingFilter resLog = new ResponseLoggingFilter();
+        RestAssured.filters(reqLog, resLog);
+    }
+}
