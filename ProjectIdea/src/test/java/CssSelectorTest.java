e1a7fa9c30c3d21f3d6b73b3b7a306c7e747f701
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-01-29 12:56:28 +0100

    another commit 'github' 29.01

--- ProjectIdea/src/test/java/CssSelectorTest.java
+++ ProjectIdea/src/test/java/CssSelectorTest.java
@@ -1,2 +1,88 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class CssSelectorTest {
+
+
+    public void findElements() {
+        WebDriver driver = new ChromeDriver();
+        //clickOnMe
+        By buttonId = By.id("clickOnMe");
+        driver.findElement(buttonId);
+
+        //fname
+        By firstName = By.name("fname");
+        driver.findElement(firstName);
+
+        By paraHidden = By.className("topSecret");
+        driver.findElement(paraHidden);
+
+        By inputLocator = By.tagName("input");
+        WebElement input = driver.findElement(inputLocator);
+        input.sendKeys("Pierwszy");
+        List<WebElement> inputs = driver.findElements(inputLocator);
+        System.out.println(inputs.size());
+
+        By linkText = By.linkText("Visit W3Schools.com!");
+        driver.findElement(linkText);
+
+        By partialLink = By.partialLinkText("visit");
+        driver.findElement(partialLink);
+
+        By cssId = By.cssSelector("#clickOnMe");
+        driver.findElement(cssId);
+
+        By cssClass = By.cssSelector(".topSecret");
+        driver.findElement(cssClass);
+
+        By cssTag = By.cssSelector("input");
+        driver.findElement(cssTag).sendKeys("Pierwszy");
+
+        By cssName = By.cssSelector("[name='fname']");
+        driver.findElement(cssName);
+
+        By cssClass2 = By.cssSelector("[class='topSecret']");
+        driver.findElement(cssClass2);
+
+        By all = By.cssSelector("*");
+        driver.findElement(all);
+
+        By ulInDiv = By.cssSelector("div ul");
+        By trInTable = By.cssSelector("table tr");
+        By trInBody = By.cssSelector("tbody > tr");
+
+        By firstChildUlinDiv = By.cssSelector("div > ul");
+        By firstChildTrInTbody = By.cssSelector("tbody > tr");
+        driver.findElement(firstChildUlinDiv);
+        driver.findElement(firstChildTrInTbody);
+
+        By firstFromAfterLabel = By.cssSelector("label + form");
+        By allformAfterLabel = By.cssSelector("label ~ form");
+        driver.findElement(firstFromAfterLabel);
+        driver.findElement(allformAfterLabel);
+
+        By attrTag = By.cssSelector("input[name='fname']");
+        By attrContains = By.cssSelector("[name*='name']");
+        By attrStarts = By.cssSelector("[name^='f']");
+        By attrEnds = By.cssSelector("[name$='name']");
+
+        driver.findElement(attrTag);
+        driver.findElement(attrContains);
+        driver.findElement(attrStarts);
+        driver.findElement(attrEnds);
+
+        By firstChild = By.cssSelector("li:first-child");
+        By lastChild = By.cssSelector("li:last-child");
+        By thirdChild = By.cssSelector("li:nth-child(3)");
+
+        driver.findElement(firstChild);
+        driver.findElement(lastChild);
+        driver.findElement(thirdChild);
+
+    }
 }
+
