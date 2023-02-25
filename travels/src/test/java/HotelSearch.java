a756f8f51ff92bb549db93ffc4af3b2de460e73b
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-02-25 23:05:06 +0100

    Set city name 25.02

---
+++ travels/src/test/java/HotelSearch.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HotelSearch {
+
+    @Test
+    public void searchHotel() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+    }
+}
