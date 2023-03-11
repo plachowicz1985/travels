bfdef5aa40fe23416d5c5b3f82effa8026dc34be
plachowicz1985 <piotr.lachowicz1985@gmail.com>
2023-03-11 22:21:34 +0100

    add LoggedUserPage refactor SignUpTest

--- travels/src/test/java/pl/seleniumdemo/pages/SignUpPage.java
+++ travels/src/test/java/pl/seleniumdemo/pages/SignUpPage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SignUpPage {
 
     @FindBy (name = "firstname")
@@ -28,6 +31,9 @@ public class SignUpPage {
 
     @FindBy (xpath = "//button[@type='submit' and text()=' Sign Up']")
     private WebElement submitButton;
+
+    @FindBy (xpath = "//form[@id='headersignupform'] // p")
+    private List<WebElement> errors;
 
 
     public SignUpPage(WebDriver driver) {
@@ -62,4 +68,10 @@ public class SignUpPage {
         submitButton.click();
     }
 
+    public List<String> getErrors() {
+
+        return errors.stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
 }
--- travels/src/test/java/pl/seleniumdemo/tests/HomeworkTest.java
+++ travels/src/test/java/pl/seleniumdemo/tests/HomeworkTest.java
@@ -4,37 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pl.seleniumdemo.pages.HotelSearchPage;
-import pl.seleniumdemo.pages.LoggedUserPage;
 import pl.seleniumdemo.pages.SignUpPage;
 
 import java.util.List;
 
-public class SignUpTest extends BaseTest {
-
-    @Test
-    public void signUpTest() {
-
-        String lastName = "Cobain";
-        int randomNumber = (int) (Math.random()*1000);
-        String email = "tester_0"+randomNumber+"@tester.pl";
-
-        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
-        hotelSearchPage.openSignUpForm();
-
-        SignUpPage signUpPage = new SignUpPage(driver);
-        signUpPage.setFirstName("Kurt");
-        signUpPage.setLastName(lastName);
-        signUpPage.setPhone("511511511");
-        signUpPage.setEmail(email);
-        signUpPage.setPassword("Piotrek");
-        signUpPage.setConfirmPassword("Piotrek");
-        signUpPage.clickSubmitButton();
-
-        LoggedUserPage loggedUserPage = new LoggedUserPage(driver);
-
-        Assert.assertTrue(loggedUserPage.getHeadingText().contains(lastName));
-        Assert.assertEquals(loggedUserPage.getHeadingText(),"Hi, Kurt Cobain");
-    }
+public class HomeworkTest extends BaseTest {
 
     @Test
     public void signUpEmptyFormTest() {
